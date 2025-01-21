a78f57095151d44ba9cafa464e75ae6aeecc2f59
Riz <87264061+rizfn@users.noreply.github.com>
2025-01-21 14:30:00 +0900

    Add (potentially still incorrect) Cpp code

--- src/diversityTransition/randomConnections.cpp
+++ src/diversityTransition/randomConnections.cpp
@@ -16,22 +16,23 @@
 constexpr int DEFAULT_N_RESOURCES = 10;
 constexpr int DEFAULT_N_CHEMICALS = 100;
 constexpr int DEFAULT_N_STEPS = 10000000;
+constexpr int DEFAULT_STOPTIME = 100000;
 constexpr int DEFAULT_N_SIMS = 4;
-constexpr double DEFAULT_GROWTH_RATE = 0.00001;
+constexpr double DEFAULT_GROWTH_RATE = 10;
 constexpr double REACTION_RATE_MIN = 0.5;
 constexpr double REACTION_RATE_MAX = 1.0;
 constexpr double SPARSITY = 0.8;
 
-void set_random_ICs(int N_RESOURCES, int N_CHEMICALS, int n_records, std::vector<std::vector<double>> &S_vals, std::vector<std::vector<double>> &X_vals, std::mt19937 &gen)
+void set_random_ICs(int N_RESOURCES, int N_CHEMICALS, std::vector<double> &S_vals, std::vector<double> &X_vals, std::mt19937 &gen)
 {
     std::uniform_real_distribution<> dis(0.0, 1.0);
 
-    S_vals.assign(N_RESOURCES, std::vector<double>(n_records, 0.0));
-    X_vals.assign(N_CHEMICALS, std::vector<double>(n_records, 0.0));
+    S_vals.assign(N_RESOURCES, 0.0);
+    X_vals.assign(N_CHEMICALS, 0.0);
 
     for (int i = 0; i < N_RESOURCES; ++i)
     {
-        S_vals[i][0] = dis(gen);
+        S_vals[i] = dis(gen);
     }
 
     std::vector<double> randomICs(N_CHEMICALS);
@@ -40,8 +41,31 @@ void set_random_ICs(int N_RESOURCES, int N_CHEMICALS, int n_records, std::vector
     double sum = std::accumulate(randomICs.begin(), randomICs.end(), 0.0);
     for (int i = 0; i < N_CHEMICALS; ++i)
     {
-        X_vals[i][0] = randomICs[i] / sum;
+        X_vals[i] = randomICs[i] / sum;
     }
+}
+
+std::pair<std::vector<std::vector<int>>, std::vector<std::vector<double>>> init_connectivity_and_reaction_rates(int N_RESOURCES, int N_CHEMICALS, double sparsity, std::mt19937 &gen)
+{
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    std::uniform_int_distribution<> int_dis(0, N_RESOURCES - 1);
+
+    std::vector<std::vector<int>> connectivity(N_CHEMICALS, std::vector<int>(N_CHEMICALS, -1));
+    std::vector<std::vector<double>> reaction_rates(N_CHEMICALS, std::vector<double>(N_CHEMICALS, 0.0));
+
+    for (int i = 0; i < N_CHEMICALS; ++i)
+    {
+        for (int j = 0; j < N_CHEMICALS; ++j)
+        {
+            if (dis(gen) < sparsity)
+            {
+                connectivity[i][j] = int_dis(gen);
+                reaction_rates[i][j] = REACTION_RATE_MIN + (REACTION_RATE_MAX - REACTION_RATE_MIN) * dis(gen);
+            }
+        }
+    }
+
+    return {connectivity, reaction_rates};
 }
 
 void ode_derivatives(const std::vector<double> &S_vals,
@@ -78,29 +102,32 @@ void ode_integrate_rk4(int N_RESOURCES,
                        int stoptime,
                        int nsteps,
                        int dataskip,
-                       std::vector<double> &T,
-                       std::vector<std::vector<double>> &S_vals,
-                       std::vector<std::vector<double>> &X_vals,
+                       std::ofstream &file,
                        std::mt19937 &gen)
 {
     double dt = static_cast<double>(stoptime) / nsteps;
-    int n_records = nsteps / dataskip + 1;
-
-    set_random_ICs(N_RESOURCES, N_CHEMICALS, n_records, S_vals, X_vals, gen);
-    T.assign(n_records, 0.0);
 
     std::vector<double> current_S(N_RESOURCES), current_X(N_CHEMICALS);
-    for (int i = 0; i < N_RESOURCES; ++i)
-        current_S[i] = S_vals[i][0];
-    for (int i = 0; i < N_CHEMICALS; ++i)
-        current_X[i] = X_vals[i][0];
+    set_random_ICs(N_RESOURCES, N_CHEMICALS, current_S, current_X, gen);
 
     std::vector<double> k1_S(N_RESOURCES), k1_X(N_CHEMICALS);
     std::vector<double> k2_S(N_RESOURCES), k2_X(N_CHEMICALS);
     std::vector<double> k3_S(N_RESOURCES), k3_X(N_CHEMICALS);
     std::vector<double> k4_S(N_RESOURCES), k4_X(N_CHEMICALS);
 
-    int record_idx = 1;
+    if (file.is_open())
+    {
+        file << "time,";
+        for (int i = 0; i < N_RESOURCES; ++i)
+        {
+            file << "resource" << i + 1 << ",";
+        }
+        for (int i = 0; i < N_CHEMICALS; ++i)
+        {
+            file << "chemical" << i + 1 << ",";
+        }
+        file << "\n";
+    }
 
     for (int i = 0; i < nsteps; ++i)
     {
@@ -126,84 +153,41 @@ void ode_integrate_rk4(int N_RESOURCES,
         ode_derivatives(S_temp, X_temp, connectivity, reaction_rates, growth_rate, k4_S, k4_X);
 
         for (int j = 0; j < N_RESOURCES; ++j)
+        {
             current_S[j] += (dt / 6.0) * (k1_S[j] + 2.0 * k2_S[j] + 2.0 * k3_S[j] + k4_S[j]);
+        }
         for (int j = 0; j < N_CHEMICALS; ++j)
+        {
             current_X[j] += (dt / 6.0) * (k1_X[j] + 2.0 * k2_X[j] + 2.0 * k3_X[j] + k4_X[j]);
+        }
 
         double sum_X = std::accumulate(current_X.begin(), current_X.end(), 0.0);
         for (int j = 0; j < N_CHEMICALS; ++j)
             current_X[j] /= sum_X;
 
-        if (i % dataskip == 0)
+        if (i % dataskip == 0 && file.is_open())
         {
-            for (int j = 0; j < N_RESOURCES; ++j)
-                S_vals[j][record_idx] = current_S[j];
-            for (int j = 0; j < N_CHEMICALS; ++j)
-                X_vals[j][record_idx] = current_X[j];
-            T[record_idx] = T[record_idx - 1] + dataskip * dt;
-            record_idx++;
-        }
-    }
-}
-
-void run_simulation(std::ofstream &file, int N_RESOURCES, int N_CHEMICALS, double growth_rate, int N_STEPS, int dataskip, const std::vector<std::vector<int>> &connectivity, const std::vector<std::vector<double>> &reaction_rates)
-{
-    std::random_device rd;
-    std::mt19937 gen(rd());
-
-    std::vector<double> T;
-    std::vector<std::vector<double>> S_vals, X_vals;
-    ode_integrate_rk4(N_RESOURCES, N_CHEMICALS, connectivity, reaction_rates, growth_rate, N_STEPS, N_STEPS, dataskip, T, S_vals, X_vals, gen);
-
-    if (file.is_open())
-    {
-        file << "step,";
-        for (int i = 0; i < N_RESOURCES; ++i)
-        {
-            file << "resource" << i + 1 << ",";
-        }
-        for (int i = 0; i < N_CHEMICALS; ++i)
-        {
-            file << "chemical" << i + 1 << ",";
-        }
-        file << "\n";
-        for (int i = 0; i < T.size(); ++i)
-        {
-            file << T[i] << ",";
+            double current_time = i * dt;
+            file << current_time << ",";
             for (int j = 0; j < N_RESOURCES; ++j)
             {
-                file << S_vals[j][i] << ",";
+                file << current_S[j] << ",";
             }
             for (int j = 0; j < N_CHEMICALS; ++j)
             {
-                file << X_vals[j][i] << ",";
+                file << current_X[j] << ",";
             }
             file << "\n";
         }
     }
 }
 
-std::pair<std::vector<std::vector<int>>, std::vector<std::vector<double>>> init_connectivity_and_reaction_rates(int N_RESOURCES, int N_CHEMICALS, double sparsity, std::mt19937 &gen)
+void run_simulation(std::ofstream &file, int N_RESOURCES, int N_CHEMICALS, double growth_rate, int stoptime, int N_STEPS, int dataskip, const std::vector<std::vector<int>> &connectivity, const std::vector<std::vector<double>> &reaction_rates)
 {
-    std::vector<std::vector<int>> connectivity(N_CHEMICALS, std::vector<int>(N_CHEMICALS, -1));
-    std::vector<std::vector<double>> reaction_rates(N_CHEMICALS, std::vector<double>(N_CHEMICALS, 0.0));
+    std::random_device rd;
+    std::mt19937 gen(rd());
 
-    std::uniform_real_distribution<> dis(0.0, 1.0);
-    std::uniform_real_distribution<> rate_dis(REACTION_RATE_MIN, REACTION_RATE_MAX);
-
-    for (int i = 0; i < N_CHEMICALS; ++i)
-    {
-        for (int j = 0; j < N_CHEMICALS; ++j)
-        {
-            if (dis(gen) >= sparsity && i != j)
-            {
-                connectivity[i][j] = std::uniform_int_distribution<>(0, N_RESOURCES - 1)(gen);
-                reaction_rates[i][j] = rate_dis(gen);
-            }
-        }
-    }
-
-    return {connectivity, reaction_rates};
+    ode_integrate_rk4(N_RESOURCES, N_CHEMICALS, connectivity, reaction_rates, growth_rate, stoptime, N_STEPS, dataskip, file, gen);
 }
 
 int main(int argc, char *argv[])
@@ -213,6 +197,7 @@ int main(int argc, char *argv[])
     double growth_rate = DEFAULT_GROWTH_RATE;
     int N_STEPS = DEFAULT_N_STEPS;
     int N_sims = DEFAULT_N_SIMS;
+    int stoptime = DEFAULT_STOPTIME;
     int dataskip = N_STEPS / 10000;
 
     if (argc > 1)
@@ -222,9 +207,11 @@ int main(int argc, char *argv[])
     if (argc > 3)
         growth_rate = std::stod(argv[3]);
     if (argc > 4)
-        N_STEPS = std::stoi(argv[4]);
+        stoptime = std::stoi(argv[4]);
     if (argc > 5)
-        N_sims = std::stoi(argv[5]);
+        N_STEPS = std::stoi(argv[5]);
+    if (argc > 6)
+        N_sims = std::stoi(argv[6]);
 
     std::string exePath = argv[0];
     std::string exeDir = std::filesystem::path(exePath).parent_path().string();
@@ -236,7 +223,7 @@ int main(int argc, char *argv[])
     std::vector<std::thread> threads;
     for (int sim = 0; sim < N_sims; ++sim)
     {
-        threads.push_back(std::thread([sim, N_RESOURCES, N_CHEMICALS, growth_rate, N_STEPS, dataskip, connectivity, reaction_rates, exeDir]()
+        threads.push_back(std::thread([sim, N_RESOURCES, N_CHEMICALS, growth_rate, stoptime, N_STEPS, dataskip, connectivity, reaction_rates, exeDir]()
         {
             std::ostringstream filePathStream;
             filePathStream << exeDir << "/outputs/randomConnections/N_" << N_RESOURCES << "-" << N_CHEMICALS << "_gamma_" << growth_rate << "_sparsity_" << SPARSITY << "_" << std::setw(2) << std::setfill('0') << sim << ".csv";
@@ -244,7 +231,7 @@ int main(int argc, char *argv[])
 
             std::ofstream file;
             file.open(filePath);
-            run_simulation(file, N_RESOURCES, N_CHEMICALS, growth_rate, N_STEPS, dataskip, connectivity, reaction_rates);
+            run_simulation(file, N_RESOURCES, N_CHEMICALS, growth_rate, stoptime, N_STEPS, dataskip, connectivity, reaction_rates);
             file.close();
         }));
     }
